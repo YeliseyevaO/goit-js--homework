a779fee07b1090425475d098cceb94a374b43ae9
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-06-13 20:17:03 +0300

    3 4 задача

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*function deliverPizza(pizzaName) {
+  return `Доставляем пиццу ${pizzaName}.`;
+}
+
+function makePizza(pizzaName) {
+  return `Пицца ${pizzaName} готовится, ожидайте...`;
+}
+
+// Пиши код ниже этой строки
+function makeMessage(pizzaName, callback) {
+  return callback(pizzaName);
+}*/
+
+function makePizza(pizzaName, callback) {
+  console.log(`Пицца ${pizzaName} готовится, ожидайте...`);
+  callback(pizzaName);
+}
+
+makePizza('Роял гранд', function deliverPizza(pizzaName) {
+  console.log(`Доставляем пиццу ${pizzaName}.`);
+});
+// Пиши код ниже этой строки
+
+makePizza('Ультрасыр', function eatPizza(pizzaName)
+{
+          consol.log(`Едим пиццу ${pizzaName}.`);
+});
