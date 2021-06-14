cc4444c9b5735d57557147bb2907381ee0a0a1f4
YeliseyevaO <eliseevaolgaf5@gmail.com>
2021-06-14 18:32:39 +0300

    закончила 4 модуль

--- src/index.js
+++ src/index.js
@@ -25,14 +25,15 @@ makePizza('Ультрасыр', function eatPizza(pizzaName)
 {
           console.log(`Едим пиццу ${pizzaName}.`);
 });*/
-const pizzaPalace = {
+/*const pizzaPalace = {
   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
-  order(pizzaName,  onSuccess, onError) {
-  if(this.pizzas.includes(pizzaName)){
-  return  onSuccess(pizzaName)}
-  
-        return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`)
-    },
+  order(pizzaName, onSuccess, onError) {
+    if (this.pizzas.includes(pizzaName)) {
+      return onSuccess(pizzaName)
+    }
+
+    return onError(`В ассортименте нет пиццы с названием ${pizzaName}.`)
+  },
 };
 // Пиши код выше этой строки
 
@@ -52,7 +53,7 @@ pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
 pizzaPalace.order('Биг майк', makePizza, onOrderError);
 pizzaPalace.order('Венская', makePizza, onOrderError)
 
-/*=========5 =======*/
+/*=========5 =======
 
 const pizzaPalace = {
   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
@@ -71,7 +72,7 @@ const pizzaPalace = {
   },
   // Пиши код выше этой строки
 };
-   /*=========6========*/
+/*=========6========
 
 const customer = {
   username: 'Mango',
@@ -104,7 +105,7 @@ customer.addOrder(5000, 'Steak');
 console.log(customer.getBalance()); // 19750
 console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
 
-/*=====7========*/
+/*=====7========
 const orders = [
   { email: 'solomon@topmail.ua', dish: 'Burger' },
   { email: 'artemis@coldmail.net', dish: 'Pizza' },
@@ -113,10 +114,43 @@ const orders = [
 
 // Пиши код ниже этой строки
 function composeMessage(position) {
-   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
 }
 
 const messages = [];
 for (let i = 0; i < orders.length; i += 1) {
   messages.push(composeMessage.call(orders[i], i + 1));
+}*/
+
+
+const service = {
+  mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+  subscribe(email) {
+    this.mailingList.push(email);
+    return `Почта ${email} добавлена в рассылку.`;
+  },
+  unsubscribe(email) {
+    this.mailingList = this.mailingList.filter((e) => e !== email);
+    return `Почта ${email} удалена из рассылки.`;
+  },
+};
+
+function logAndInvokeAction(email, action) {
+  console.log(`Выполняем действие с ${email}.`);
+  return action(email);
 }
+
+const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service));
+console.log(firstInvoke);
+// Почта kiwi@mail.uk добавлена в рассылку.
+
+console.log(service.mailingList);
+/* ['mango@mail.com', 
+    'poly@hotmail.de', 
+    'ajax@jmail.net', 
+    'kiwi@mail.uk']*/
+const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+console.log(secondInvoke);
+// Почта poly@hotmail.de удалена из рассылки.
+
+console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
